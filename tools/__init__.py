59df7e105addeec7887fd8bf4a8dde165cde9e69
Nikhil992513 <nikhilreddy.malireddy@gmail.com>
2026-03-14 10:13:56 +0530

    Revert "Revert "added new 2 tools""

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,12 +2,16 @@
 Shapefile processing tools.
 """
 
+
 from .shapefile_to_csv import ShapefileToCSVTool
 from .merge_shapefiles import MergeShapefilesTool
 from .add_shapefiles import AddShapefilesTool
 from .reproject_shapefile import ReprojectShapefileTool
 from .excel_to_csv import ExcelToCSVTool
 from .delete_duplicate_geometries import DeleteDuplicateGeometriesTool
+
+from .add_uuid_to_shapefile import AddUUIDToShapefileTool
+from .latlong_to_decimal_utm import LatLongToDecimalUTMTool
 
 __all__ = [
     "ShapefileToCSVTool",
@@ -16,4 +20,6 @@ __all__ = [
     "ReprojectShapefileTool",
     "ExcelToCSVTool",
     "DeleteDuplicateGeometriesTool",
+    "AddUUIDToShapefileTool",
+    "LatLongToDecimalUTMTool",
 ]
